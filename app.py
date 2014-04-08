50fb0b394fda4ac131f8d0630be92c93cb826715
Mihir Singh <me@mihirsingh.com>
2014-04-08 02:37:24 -0400

    Implemented file uploads for custom ambient noises

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+import os
 
 raindrop = Flask(__name__)
 
@@ -8,10 +9,23 @@ def home():
     return render_template('index.html')
 
 
-@raindrop.route('/stream/<condition>')
+@raindrop.route('/stream/<condition>', methods=['GET', 'POST'])
 def stream(condition):
 
-    return send_from_directory('sounds', condition + '.mp3')
+    if request.method == 'GET':
+
+        return send_from_directory('sounds', condition + '.mp3')
+
+    elif request.method == 'POST':
+
+        file = request.files['clip']
+
+        if file:
+
+            file.save(os.path.join('sounds', condition + '.mp3'))
+
+        return ''
+
 
 if __name__ == "__main__":
 
