e424bebbef9c1ee9c3a18cb7379f658d3a6ef20a
Mihir Singh <citruspi@users.noreply.github.com>
2014-05-08 12:44:04 -0400

    Add the long description #12

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
 	version = '0.1',
 	packages = ['raindrop'],
 	description = "Ambient music based on the current weather",
+	long_description = "A python/flask application that will play ambient music based on your current weather. The project does not come with music, as the user will set the ambient music tracks themselves. Music files need to be in .mp3 format.",
 	include_package_data = True,
 	zip_safe = False,
 	scripts = [ "raindrop/bin/raindrop" ],
