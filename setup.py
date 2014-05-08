249f5d0e86029492264bf7c80d42e61434b47ab0
Mihir Singh <citruspi@users.noreply.github.com>
2014-05-08 12:42:59 -0400

    Add the description #11

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
 	name = 'raindrop.py',
 	version = '0.1',
 	packages = ['raindrop'],
+	description = "Ambient music based on the current weather",
 	include_package_data = True,
 	zip_safe = False,
 	scripts = [ "raindrop/bin/raindrop" ],
